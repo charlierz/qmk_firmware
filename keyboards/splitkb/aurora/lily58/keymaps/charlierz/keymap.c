c3764418901b72cffa0a82d730c5eb6746dc2f25
Carlos Rincon <carlos.rincon.zapata@gmail.com>
2024-03-11 23:28:44 +0100

    hrm working

--- keyboards/splitkb/aurora/lily58/keymaps/charlierz/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/charlierz/keymap.c
@@ -9,7 +9,7 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO, KC_NO, LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_NO, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_ENT, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_LCTL, LT(2,KC_ESC), LT(1,KC_TAB), KC_SPC, KC_ENT, LT(2,KC_BSPC), LT(1,KC_DEL), TO(3)),
+	[0] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO, KC_NO, LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_NO, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_ENT, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_LCTL, LT(2,KC_ESC), LT(1,KC_TAB), KC_SPC, RSFT_T(KC_ENT), LT(1,KC_BSPC), LT(2,KC_DEL), TO(3)),
 	[1] = LAYOUT(KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_NO, KC_LT, KC_SLSH, KC_LCBR, KC_RCBR, KC_GT, KC_CIRC, KC_AT, KC_HASH, KC_BSLS, KC_TILD, KC_F12, KC_NO, KC_MINS, KC_UNDS, KC_LPRN, KC_RPRN, KC_COLN, KC_DLR, KC_EQL, KC_DQUO, KC_QUOT, KC_GRV, KC_NO, KC_NO, KC_PLUS, KC_EXLM, KC_LBRC, KC_RBRC, KC_NO, KC_TRNS, KC_NO, KC_PERC, KC_ASTR, KC_AMPR, KC_PIPE, KC_QUES, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO),
 	[2] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAI, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_NUM, KC_PSCR, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, RGB_VAD, KC_NO, KC_PMNS, KC_P4, KC_P5, KC_P6, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_VOLD, RGB_TOG, KC_NO, KC_PPLS, KC_P1, KC_P2, KC_P3, KC_P0, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MUTE, KC_NO, KC_NO, KC_P0, KC_PDOT, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO),
 	[3] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_ENT, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_LALT, KC_SPC, KC_LCTL, KC_SPC, KC_NO, KC_NO, KC_NO, TG(3))
@@ -30,4 +30,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 void matrix_scan_user(void) {
   achordion_task();
+}
+
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+
+   // Also allow same-hand holds when the other key is in the rows below the
+  // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
+  if (tap_hold_record->event.key.row % (MATRIX_ROWS / 2) == 4) { return true; }                      
+       
+  // Otherwise, follow the opposite hands rule.
+  return achordion_opposite_hands(tap_hold_record, other_record);
 }
