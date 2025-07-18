b3b0c9c2078b48dbf1e12e17ba04330f080db424
charlierz <carlos.rincon.zapata@gmail.com>
2025-07-18 19:30:47 +0200

    fix: hyper on linux

--- keyboards/splitkb/aurora/lily58/keymaps/charlierz/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/charlierz/keymap.c
@@ -4,7 +4,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
         KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,            KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
         CW_TOGG, KC_Q, KC_W, KC_E, KC_R, KC_T,          KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RGUI,
-        OSM(MOD_HYPR), KC_A, KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K, KC_L, KC_COLN, KC_MINS,
+        OS_LCSG, KC_A, KC_S, KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K, KC_L, KC_COLN, KC_MINS,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_ENT,  TO(3), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
         KC_LGUI, LT(2,KC_TAB), LCTL_T(KC_ESC), KC_SPC,  KC_ENT, LT(1,KC_BSPC), LALT_T(KC_DEL), KC_NO
     ),
@@ -30,12 +30,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ENT, KC_LALT, KC_LCTL, KC_SPC,             KC_ENT, KC_BSPC, KC_DEL, KC_RGUI
     )
 };
-
-#if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-
-};
-#endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 
 void keyboard_pre_init_user(void) {
   // Set our LED pin as output
